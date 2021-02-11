fcc93e38a6c31ad0bbd41290029cd57a64748a34
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-11 12:32:00 +0000

    Refactoring of test to follow DRY principle

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,19 +1,22 @@
 require 'secret_diary'
 
+
 describe SecretDiary do
-  it "locks the diary by default" do
-    secret_diary = SecretDiary.new
-    expect { secret_diary.add_entry }.to raise_error 'SecretDiary is locked!'
+  before(:each) do
+    @secret_diary = SecretDiary.new
   end
-  it "let's you add an entry if you unlock it" do
-    secret_diary = SecretDiary.new
-    secret_diary.unlock
-    expect { secret_diary.add_entry }.not_to raise_error
-  end
-  it "raises an error when user calls lock" do
-    secret_diary = SecretDiary.new
-    secret_diary.unlock
-    secret_diary.lock
-    expect { secret_diary.add_entry }.to raise_error 'SecretDiary is locked!'
+  describe '#add_entry' do
+    it "locks the diary by default" do
+      expect { @secret_diary.add_entry }.to raise_error 'SecretDiary is locked!'
+    end
+    it "let's you add an entry if you unlock it" do
+      @secret_diary.unlock
+      expect { @secret_diary.add_entry }.not_to raise_error
+    end
+    it "raises an error when user calls lock" do
+      @secret_diary.unlock
+      @secret_diary.lock
+      expect { @secret_diary.add_entry }.to raise_error 'SecretDiary is locked!'
+    end
   end
 end
