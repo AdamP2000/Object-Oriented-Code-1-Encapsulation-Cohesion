758eec484883bc735b8e36edeb0008072da673d8
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-11 12:04:51 +0000

    Passing unit test when user calls unlock add_entry doesn't raise error

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,11 +1,20 @@
 class SecretDiary
 
+  def initialize
+    @locked = true
+  end
+
 
 
 
 
   def add_entry
-    fail 'SecretDiary is locked!'
+    fail 'SecretDiary is locked!' if @locked == true
+    "Coming back to how to add entries later"
+  end
+
+  def unlock
+    @locked = false
   end
 
 
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -5,4 +5,9 @@ describe SecretDiary do
     secret_diary = SecretDiary.new
     expect { secret_diary.add_entry }.to raise_error 'SecretDiary is locked!'
   end
+  it "let's you add an entry if you unlock it" do
+    secret_diary = SecretDiary.new
+    secret_diary.unlock
+    expect { secret_diary.add_entry }.not_to raise_error
+  end
 end
